7f89c0843543fb127024c463ec2134b714311f69
Srinivas Gattu <srinivas@callippus.co.uk>
2015-03-24 21:37:52 -0700

    Changed case of BAPI as server was returning null

--- src/main/java/org/hibersap/forge/GenerateSAPEntitiesPlugin.java
+++ src/main/java/org/hibersap/forge/GenerateSAPEntitiesPlugin.java
@@ -213,7 +213,7 @@ public final class GenerateSAPEntitiesPlugin implements Plugin {
 		// "\nSelect a function to generate the necessary Java classes:",
 		// functionNames );
 
-		final String[] functionNames = new String[] { "Zportalorder_createfromdat2" };
+		final String[] functionNames = new String[] { "ZPORTALORDER_CREATEFROMDAT2" };
 
 		for (String functionName : functionNames) {
 			if (!functionName.equals("Cancel")) {
